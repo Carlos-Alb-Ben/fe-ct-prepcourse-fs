1088f861fababd3923ff8bb92b0f55b98cf6bb6e
Carlos-Alb-Ben <cbenitez_1976@hotmail.com.ar>
2023-08-30 06:52:49 -0300

    agrego comillas simples al string

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = "Carlos"; 
+const nuevoString = 'Carlos'; 
 
 // Crea una variable de tipo number.
 const nuevoNumero = 1;
